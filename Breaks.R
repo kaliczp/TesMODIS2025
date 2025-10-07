e018797beb2965f061d5a7e5a50ae16099663fa5
Péter Kalicz <kaliczp@gmail.com>
2025-10-07 16:51:05 +0200

    Save QA and archive data

--- Breaks.R
+++ Breaks.R
@@ -33,3 +33,21 @@ VI_Tes <- mt_subset(product = "MOD13Q1",
                        site_name = "Tes",
                        internal = TRUE,
                        progress = TRUE)
+
+## Pixel reliability
+QA_Tes <- mt_subset(product = "MOD13Q1",
+                    lat = 47.270344,
+                    lon = 18.04669,
+                    band = "250m_16_days_pixel_reliability",
+                    start = "2001-01-01",
+                    end = "2025-09-30",
+                    km_lr = 1,
+                    km_ab = 1,
+                    site_name = "Tes",
+                    internal = TRUE,
+                    progress = TRUE)
+
+## Save for emergency
+save(VI_Tes, file = "VI_Tes.RData")
+save(QA_Tes, file = "QA_Tes.RData")
+
