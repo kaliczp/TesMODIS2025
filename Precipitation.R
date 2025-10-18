87c3cdca123bb5fcd8d4d5b6ab4e42447977c6fc
Péter Kalicz <kaliczp@gmail.com>
2025-10-18 12:09:39 +0200

    Timeseries with xts instead zoo

--- Precipitation.R
+++ Precipitation.R
@@ -4,7 +4,9 @@ unzip("HABP_1H_35116_20020101_20241231_hist.zip")
 TesClimateFull <- read.table("HABP_1H_20020101_20241231_35116.csv", header = TRUE, sep = ";")
 ## After description data given in UTC
 Sys.setenv(TZ="UTC")
-PrecHourly <- zoo(TesClimateFull$r,as.POSIXct(as.character(TesClimateFull$Time), format = "%Y%m%d%H%M"))
+## install.packages("xts") # If necessary
+library(xts) # load xts
+PrecHourly <- xts(TesClimateFull$r,as.POSIXct(as.character(TesClimateFull$Time), format = "%Y%m%d%H%M"))
 ## Remove possible NAs
 PrecHourly[PrecHourly < -10] <- NA
-plot(PrecHourly, xaxs = "i")
+plot(PrecHourly)
