f75e88606647cdea6086aa52279205e3f998524f
Péter Kalicz <kaliczp@gmail.com>
2025-10-18 17:11:52 +0200

    Water year precipitation
    Cummulated hyetogpraphs for water years, from 1st of November. This commit resolves #4 issue.

--- Precipitation.R
+++ Precipitation.R
@@ -35,3 +35,32 @@ ttyear <- 2022
              xlab = "", xaxt = "n",
              xaxs = "i", yaxs = "i")
 }
+
+## For water year
+dev.off()
+for(ttyear in 2002:2023) {
+    par(new = TRUE)
+    WaterYear <- paste0(ttyear, "-11-01/", ttyear + 1, "-10-31")
+    plot.zoo(cumsum(PrecHourly[WaterYear]),
+             ylim = c(0,1200),
+             xlab = "", xaxt = "n",
+             ylab = "", yaxt = "n",
+             xaxs = "i", yaxs = "i")
+}
+TimeForAxis <- c(ISOdate(ttyear,11:12,1), ISOdate(ttyear+1,01:11,1))
+axis(1, at = TimeForAxis , lab = FALSE)
+axis.POSIXct(1, at = TimeForAxis + 15*24*60*60, format = "%b.", tcl = 0)
+axis(2)
+
+ttyear <- 2022
+par(new = TRUE)
+WaterYear <- paste0(ttyear, "-11-01/", ttyear + 1, "-10-31")
+PrecCumsum <- cumsum(PrecHourly[WaterYear])
+cat(paste0(PrecCumsum[length(PrecCumsum)],"\n"))
+plot.zoo(PrecCumsum,
+         ylim = c(0,1200),
+         col = "red", lwd = 4,
+         xlab = "", xaxt = "n",
+         xaxs = "i", yaxs = "i"
+         )
+}
