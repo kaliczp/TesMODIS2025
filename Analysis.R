1aca62170550f240679c36693c084c0cf89346ff
Péter Kalicz <kaliczp@gmail.com>
2025-10-29 09:35:49 +0100

    y ax labe and structuring

--- Analysis.R
+++ Analysis.R
@@ -1,3 +1,5 @@
+library(zoo)
+
 ## For 41 pixel
 px <- 41 # pixel number, estimated based on above map
 tspx <- timeser(unlist(VI_m[px]),as.Date(names(VI_m), "%Y-%m-%d")) # convert pixel "px" to a time series
@@ -8,32 +10,38 @@ plot(tspx, xlab = "", ylab = "VI") # NDVI time series cleaned using the "reliabi
 dev.off()
 
 dev.off()
+
+## Összes év vékony feketén
 for(ido in 2003:2021) {
     par(new = TRUE)
     plot(window(tspx,ido,ido+1),
          xlim = c(ido, ido+1),
          ylim = c(0.2, 1),
-         xaxt = "n", xlab = ""
+         xaxt = "n", xlab = "", ylab = ""
          )
 }
+
+## Az egyes évek az összes évre nyomtatása
 ido <- 2022
 par(new = TRUE)
 plot(window(tspx,ido,ido+1), lwd = 4, col = "red",
      xlim = c(ido, ido+1),
      ylim = c(0.2, 1),
-     xaxt = "n", xlab = ""
+     xaxt = "n", xlab = "", ylab = "NDVI"
      )
+
 ido <- 2001
 par(new = TRUE)
 plot(window(tspx,ido,ido+1), lwd = 4, col = "blue",
      xlim = c(ido, ido+1),
      ylim = c(0.2, 1),
-     xaxt = "n", xlab = ""
+     xaxt = "n", xlab = "", ylab = "NDVI"
      )
+
 ido <- 2002
 par(new = TRUE)
 plot(window(tspx,ido,ido+1), lwd = 4, col = "green",
      xlim = c(ido, ido+1),
      ylim = c(0.2, 1),
-     xaxt = "n", xlab = ""
+     xaxt = "n", xlab = "", ylab = "NDVI"
      )
