9b727bf33d408c550eedb742943d159dd07e5f61
Péter Kalicz <kaliczp@gmail.com>
2025-10-30 10:38:55 +0100

    More fitted figure

--- ModisGrayFig.R
+++ ModisGrayFig.R
@@ -1,22 +1,23 @@
 library(scales)
 
-png()
+png("MODISgray.png", width = 6, height = 5, units = "cm", res = 300, pointsize = 10)
+par(mar = c(2.1,4.1,0.2,0.2), las = 1)
 ido <- 2003
 tspxakt <- window(tspx,ido,ido+1)
 tspxNINCSido <- ts(as.vector(tspxakt), start=0, end=1, frequency = 23)
 plot(tspxNINCSido,
      xlim = c(0,1),
      ylim = c(0.2, 1),
-     xaxt = "n", xlab = "", ylab = "",
+     xaxt = "n", xlab = "", ylab = "NDVI",
      col = alpha("black", 0.5),
-     lwd = 5
+     lwd = 3
      )
 for(ido in 2004:2021) {
     tspxakt <- window(tspx,ido,ido+1)
     tspxNINCSido <- ts(as.vector(tspxakt), start=0, end=1, frequency = 23)
     lines(tspxNINCSido,
           col = alpha("black", 0.5),
-          lwd = 5
+          lwd = 3
           )
 }
 ido <- 2003
@@ -24,6 +25,6 @@ tspxakt <- window(tspx,ido,ido+1)
 tspxNINCSido <- ts(as.vector(tspxakt), start=0, end=1, frequency = 23)
 lines(tspxNINCSido,
       col = "red",
-      lwd = 5
+      lwd = 3
       )
 dev.off()
